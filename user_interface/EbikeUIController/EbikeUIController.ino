93503df3311a80aa283e27a3a83630f9d92310af
Chris Hyggen <chyggenot@gmail.com>
2024-07-17 16:19:33 -0700

    project restructure

--- user_interface/EbikeUIController/EbikeUIController.ino
+++ user_interface/EbikeUIController/EbikeUIController.ino
@@ -1,13 +1,115 @@
 #include "functions.h"
+#include "logging.h"
+#include <TimerOne.h>   // Include for Timer ISR configuration
+#include <SPI.h>
+
+// Global variables - only to be accessed within this file
+int speed = 0;                    // Current speed
+int throttle = 0;                 // Current throttle percentage
+int power = 0;                    // Current power value
+int temp = 0;                     // Current temperature
+int batterySOC = 0;               // Battery state of charge of the e-bike
+int regenMethod = 1;              // State of active regenerative braking
+bool activatedRegen = false;           // Regenerative braking engaged
+bool logging_enabled;
+
+
+// State variables for debouncings
+volatile bool previousRegenState = HIGH; // Previous state of the REGEN_METHOD_TOGGLE pin
+volatile bool currentIncreaseThrottleState = LOW; // Increase throttle request
+volatile bool currentDecreaseThrottleState = LOW; // Increase throttle request
+volatile bool previousIncreaseThrottleState = HIGH; // Previous state of the REGEN_METHOD_TOGGLE pin
+volatile bool previousDecreaseThrottleState = HIGH; // Previous state of the REGEN_METHOD_TOGGLE pin
+
+// Can stuff
+CAN_message_t msg;                // CAN message structure
+int throttle_flag = 0;            // Flag to indicate if throttle value has changed
+
+// declaration of timer ISR 
+void timerISR();
 
 void setup() {
+  // Activating logging is breaking the screen SPI, commented out for now
+  // logging_enabled = loggingInit(LOGGING_CS_PIN);
   displayInit();
   // canInit(); Commented out until CAN functionality is integrated to avoid compilation errors
   pinModesInit();
-  timerISRInit();
   Serial.begin(9600);
+
+
+  Timer1.initialize(10000); // Initialize Timer1 to trigger ISR at 100 Hz
+  Timer1.attachInterrupt(timerISR); // Attach timerISR function to begin the timerISR loop
 }
 
 void loop() {
   // The loop function is left empty as all processing is done in the ISR
+}
+
+void timerISR() {
+  // Static variables to store the last time an event was handled
+  static unsigned long lastThrottleTime = 0;
+  static unsigned long lastRegenTime = 0;
+  unsigned long currentTime = millis(); // Get the current time in milliseconds
+
+  // Poll THROTTLE_SPEED_INPUT pin (Analog read)
+  int currentThrottleValue = analogRead(THROTTLE_SPEED_INPUT);
+
+  // Determine throttle state based on current throttle value
+  if (currentThrottleValue < 20) {
+    currentIncreaseThrottleState = HIGH;  // Increase throttle request
+    currentDecreaseThrottleState = LOW;
+  } else if (currentThrottleValue > 52 && currentThrottleValue < 58) {
+    currentIncreaseThrottleState = LOW;   // Decrease throttle request
+    currentDecreaseThrottleState = HIGH;
+  } else {
+    currentIncreaseThrottleState = LOW;   // No throttle change
+    currentDecreaseThrottleState = LOW;
+    throttle_flag = 0;
+  }
+
+  // Check for throttle increase with debouncing
+  if (currentIncreaseThrottleState != previousIncreaseThrottleState &&
+      currentIncreaseThrottleState == HIGH &&
+      currentTime - lastThrottleTime > 25) { // Debounce time of 25ms
+    handleThrottleInput(1, throttle, activatedRegen);  // Increase throttle request
+    lastThrottleTime = currentTime; // Update last throttle event time
+  }
+
+  // Check for throttle decrease with debouncing
+  if (currentDecreaseThrottleState != previousDecreaseThrottleState &&
+      currentDecreaseThrottleState == HIGH &&
+      currentTime - lastThrottleTime > 25) { // Debounce time of 25ms
+    handleThrottleInput(2, throttle, activatedRegen); // Decrease throttle request
+    lastThrottleTime = currentTime; // Update last throttle event time
+  }
+
+  // Update previous throttle state for next comparison
+  previousIncreaseThrottleState = currentIncreaseThrottleState;
+  previousDecreaseThrottleState = currentDecreaseThrottleState;
+
+  // Poll REGEN_METHOD_TOGGLE pin (Digital read)
+  bool currentRegenState = digitalRead(REGEN_METHOD_TOGGLE);
+  if (currentRegenState == LOW && previousRegenState == HIGH) {
+    // Detect falling edge and check debounce time
+    if (currentTime - lastRegenTime > 25) { // Debounce time of 25ms
+      regenMethod = (regenMethod == 1) ? 2 : 1; // Toggle between regen method 1 and 2
+      lastRegenTime = currentTime; // Update last regen event time
+    }
+  }
+  previousRegenState = currentRegenState; // Update previous regen state
+
+  // Poll E-BRAKE_ACTIVATED pin (Digital read)
+  bool currentBrakeState = digitalRead(E_BRAKE_ENGAGED);
+  if (currentBrakeState == LOW) {
+    // As long as the brake is engaged, activate regen
+    handleThrottleInput(3, throttle, activatedRegen); // Ebrake request
+  } else {
+    if (activatedRegen) {
+      throttle = 0; // Reset throttle to zero when eBrake is released
+      activatedRegen = false; // When the brake is released, deactivate regen
+    }
+  }
+
+  // Update display with current status
+  updateDisplay(throttle, speed, power, temp, batterySOC, regenMethod);
 }
--- user_interface/EbikeUIController/functions.cpp
+++ user_interface/EbikeUIController/functions.cpp
@@ -1,9 +1,34 @@
-#include "globals.h"
 #include "functions.h"
 #include "bitmaps.h"
+////////////////////
+// SCREEN GLOBALS // 
+////////////////////
+// Create an instance for a 128x64 display with software SPI
+static U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clk=*/ OLED_CLK, /* data=*/ OLED_MOSI, /* cs=*/ OLED_CS, /* dc=*/ OLED_DC, /* reset=*/ OLED_RESET);
 
-#include <FlexCAN_T4.h> // Include for CAN communication
-#include <TimerOne.h>   // Include for Timer ISR configuration
+
+// String buffers for displaying values
+char speed_string[10];            // String to display speed
+int speed_string_length;          // Length of the speed string
+
+char throttle_string[10];         // String to display throttle value
+int throttle_string_length;       // Length of the throttle string
+
+char power_string[10];            // String to display power value
+int power_string_length;          // Length of the power string
+
+char temp_string[10];             // String to display temperature
+int temp_string_length;           // Length of the temperature string
+
+char battery_string[10];          // String to display battery range
+int battery_string_length;        // Length of the battery string
+
+/////////////////
+// CAN GLOBALS //
+/////////////////
+
+// CAN2.0 declaration - CAN0 port on Teensy 3.2
+FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> can0;
 
 // Function to initialize the OLED display
 void displayInit() {
@@ -16,23 +41,24 @@ void displayInit() {
   } while (u8g2.nextPage());
 
   delay(4000); // Wait for 4 seconds to show the logo
+  updateDisplay(0,0,0,0,0,0);
 }
 
 // Function to initialize the CAN communication
-void canInit() {
-  Serial.begin(115200); // Set baud rate to 115200
-  can0.begin(); // CAN initialization
-  can0.setBaudRate(500000); // Set CAN baud rate to 500kbit
+// void canInit() {
+//   Serial.begin(115200); // Set baud rate to 115200
+//   can0.begin(); // CAN initialization
+//   can0.setBaudRate(500000); // Set CAN baud rate to 500kbit
 
-  can0.setMBFilter(REJECT_ALL);
-  can0.setMBFilter(MB0, system_params_ID); // Set mailbox CAN ID filters
-  can0.enableMBInterrupt(MB0); // Enable mailbox interrupts
-  can0.onReceive(MB0, updateSystemParams); // Set mailbox 0 to receive system parameters, assign updateSystemParams function
-  can0.setMB(MB1, TX); // Set mailbox 1 as transmit
+//   can0.setMBFilter(REJECT_ALL);
+//   can0.setMBFilter(MB0, SYSTEM_PARAMS_ID); // Set mailbox CAN ID filters
+//   can0.enableMBInterrupt(MB0); // Enable mailbox interrupts
+//   can0.onReceive(MB0, updateSystemParams); // Set mailbox 0 to receive system parameters, assign updateSystemParams function
+//   can0.setMB(MB1, TX); // Set mailbox 1 as transmit
   
-  msg.id = 0x333; // Set CAN ID 0x333 to throttle/regen info 
-  msg.len = 8; // Data length set to 8 bytes
-}
+//   msg.id = 0x333; // Set CAN ID 0x333 to throttle/regen info 
+//   msg.len = 8; // Data length set to 8 bytes
+// }
 
 // Function to set pin modes
 void pinModesInit() {
@@ -41,131 +67,67 @@ void pinModesInit() {
   pinMode(THROTTLE_SPEED_INPUT, INPUT); // Analog input pin does not need pull-up
 }
 
-// Function to initialize Timer1 and attach ISR
-void timerISRInit() {
-  Timer1.initialize(10000); // Initialize Timer1 to trigger ISR at 100 Hz
-  Timer1.attachInterrupt(timerISR); // Attach timerISR function
-}
+// // Function to initialize Timer1 and attach ISR
+// void timerISRInit() {
+//   Timer1.initialize(10000); // Initialize Timer1 to trigger ISR at 100 Hz
+//   Timer1.attachInterrupt(timerISR); // Attach timerISR function
+// }
 
-void timerISR() {
-  // Static variables to store the last time an event was handled
-  static unsigned long lastThrottleTime = 0;
-  static unsigned long lastRegenTime = 0;
-  unsigned long currentTime = millis(); // Get the current time in milliseconds
-
-  // Poll THROTTLE_SPEED_INPUT pin (Analog read)
-  int currentThrottleValue = analogRead(THROTTLE_SPEED_INPUT);
-
-  // Determine throttle state based on current throttle value
-  if (currentThrottleValue < 20) {
-    currentIncreaseThrottleState = HIGH;  // Increase throttle request
-    currentDecreaseThrottleState = LOW;
-  } else if (currentThrottleValue > 52 && currentThrottleValue < 58) {
-    currentIncreaseThrottleState = LOW;   // Decrease throttle request
-    currentDecreaseThrottleState = HIGH;
-  } else {
-    currentIncreaseThrottleState = LOW;   // No throttle change
-    currentDecreaseThrottleState = LOW;
-    throttle_flag = 0;
-  }
-
-  // Check for throttle increase with debouncing
-  if (currentIncreaseThrottleState != previousIncreaseThrottleState &&
-      currentIncreaseThrottleState == HIGH &&
-      currentTime - lastThrottleTime > 25) { // Debounce time of 25ms
-    handleThrottleInput(1);  // Increase throttle request
-    lastThrottleTime = currentTime; // Update last throttle event time
-  }
-
-  // Check for throttle decrease with debouncing
-  if (currentDecreaseThrottleState != previousDecreaseThrottleState &&
-      currentDecreaseThrottleState == HIGH &&
-      currentTime - lastThrottleTime > 25) { // Debounce time of 25ms
-    handleThrottleInput(2); // Decrease throttle request
-    lastThrottleTime = currentTime; // Update last throttle event time
-  }
-
-  // Update previous throttle state for next comparison
-  previousIncreaseThrottleState = currentIncreaseThrottleState;
-  previousDecreaseThrottleState = currentDecreaseThrottleState;
-
-  // Poll REGEN_METHOD_TOGGLE pin (Digital read)
-  bool currentRegenState = digitalRead(REGEN_METHOD_TOGGLE);
-  if (currentRegenState == LOW && previousRegenState == HIGH) {
-    // Detect falling edge and check debounce time
-    if (currentTime - lastRegenTime > 25) { // Debounce time of 25ms
-      regenMethod = (regenMethod == 1) ? 2 : 1; // Toggle between regen method 1 and 2
-      lastRegenTime = currentTime; // Update last regen event time
-    }
-  }
-  previousRegenState = currentRegenState; // Update previous regen state
-
-  // Poll E-BRAKE_ACTIVATED pin (Digital read)
-  bool currentBrakeState = digitalRead(E_BRAKE_ENGAGED);
-  if (currentBrakeState == LOW) {
-    // As long as the brake is engaged, activate regen
-    handleThrottleInput(3);
-  } else {
-    if (activatedRegen == 1) {
-      throttle = 0; // Reset throttle to zero when eBrake is released
-      activatedRegen = 0; // When the brake is released, deactivate regen
-    }
-  }
-
-  // Update display with current status
-  updateDisplay();
-}
-
-void handleThrottleInput(int inputRequest) {
+bool handleThrottleInput(int inputRequest, int &throttle, bool &activatedRegen) {
+  bool throttle_changed = false;
   if (inputRequest == 1) {
     // Increase throttle if not at maximum
     if (throttle == 100) {
-      return;
+      return false;
     } else {
       throttle += 5; // Increase throttle by 20
-      throttle_flag = 1; // Set throttle flag
+      throttle_changed = 1; // Set throttle flag
     }
   } else if (inputRequest == 2) {
     // Decrease throttle if not at minimum
     if (throttle == 0) {
-      return;
+      return false;
     } else {
       throttle -= 5; // Decrease throttle by 20
-      throttle_flag = 1; // Set throttle flag
+      throttle_changed = 1; // Set throttle flag
     }
   }
+
   else if (inputRequest == 3) {
     throttle = -100;
-    activatedRegen = 1;
-    throttle_flag = 1;
+    activatedRegen = true;
+    throttle_changed = 1;
   }
   // // Check if any flag is set to send CAN message
-  // if (throttle_flag == 1) {
+  // if (throttle_changed == 1) {
   //   sendCANMSG(); // Send CAN message if any flag is set
+  //   throttle_changed = 0;
   // } Commented out until CAN functionality is integrated to avoid compilation errors
+
+  return throttle_changed;
 }
 
 // Function to send CAN messages
-void sendCANMSG(void) {
-
+void sendCANMSG(int throttle) {
+  CAN_message_t msg;
   msg.buf[0] = throttle & 0xFF; // Throttle value in byte 0
   can0.write(MB1, msg); // Send message to mailbox 1 (transmit mailbox)
 
-  // Reset flags after sending the message
-  throttle_flag = 0;
+  // // Reset flags after sending the message
+  // throttle_flag = 0;
 }
 
 // Function to update system parameters from CAN messages
-void updateSystemParams(const CAN_message_t &msg, int &speed) {
-  speed = (msg.buf[1] << 8) | msg.buf[0]; // Speed allocated to bytes 0-1
-  power = (msg.buf[3] << 8) | msg.buf[2]; // Power allocated to bytes 2-3
-  temp = msg.buf[4]; // Temperature allocated to byte 4
-  batterySOC = (msg.buf[6] << 8) | msg.buf[5]; // Battery SOC allocated to bytes 5-6
+void updateSystemParams(const CAN_message_t &msg) {
+  // speed = (msg.buf[1] << 8) | msg.buf[0]; // Speed allocated to bytes 0-1
+  // power = (msg.buf[3] << 8) | msg.buf[2]; // Power allocated to bytes 2-3
+  // temp = msg.buf[4]; // Temperature allocated to byte 4
+  // batterySOC = (msg.buf[6] << 8) | msg.buf[5]; // Battery SOC allocated to bytes 5-6
 }
 
 // Function to update the display with the latest values
-void updateDisplay() {
-
+void updateDisplay(int throttle, int speed, int power, int temp, int batterySOC, int regenMethod) {
+  bool negativeThrottle;
   // Handle negative throttle values
   if (throttle < 0) {
     itoa(throttle * -1, throttle_string, 10);
@@ -186,10 +148,11 @@ void updateDisplay() {
   power_string_length = strlen(power_string);
   temp_string_length = strlen(temp_string);
   battery_string_length = strlen(battery_string);
-
+  
   // Start the display update process
   u8g2.firstPage();
   do {
+
     // Draw speed value on the display
     for (int i = 0; i < speed_string_length; i++) {
       u8g2.drawBitmap((56 - speed_string_length * 28) + 30 * i, 2, 24 / 8, 36, epd_bitmap_speedDigitsArray[speed_string[i] - 48]);
